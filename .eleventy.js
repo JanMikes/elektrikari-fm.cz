f3733a03553bb3f7c414267ceb82d920e956dab9
Jan Mikeš <j.mikes@me.com>
2024-11-11 22:20:06 +0100

    Styles - bootstrap

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 module.exports = function(eleventyConfig) {
     // Copy CSS directory to output
-    eleventyConfig.addPassthroughCopy("styles");
+    eleventyConfig.addPassthroughCopy({
+        "node_modules/bootstrap/dist/css/bootstrap.min.css": "styles/bootstrap.css",
+    });
+    eleventyConfig.addPassthroughCopy("src/styles");
 
     // Copy Bootstrap JS to output (optional if you need JS features like modals)
     eleventyConfig.addPassthroughCopy({
