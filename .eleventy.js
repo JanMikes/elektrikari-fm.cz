505660182633efc581dd123f80bba94ffc8e78bf
Jan Mikeš <j.mikes@me.com>
2024-11-12 00:17:11 +0100

    Using webpixels

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,15 @@
 module.exports = function(eleventyConfig) {
-    // Copy CSS directory to output
     eleventyConfig.addPassthroughCopy({
+        "src/styles": "styles",
         "node_modules/bootstrap/dist/css/bootstrap.min.css": "styles/bootstrap.css",
-    });
-    eleventyConfig.addPassthroughCopy("src/styles");
-
-    // Copy Bootstrap JS to output (optional if you need JS features like modals)
-    eleventyConfig.addPassthroughCopy({
+        "node_modules/@webpixels/css/dist/index.css": "styles/webpixels.css",
         "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js": "js/bootstrap.bundle.min.js"
     });
 
     return {
         dir: {
-            input: "src",    // Your input folder (e.g., "src")
-            output: "_site"  // Your output folder
+            input: "src",
+            output: "_site"
         }
     };
 };
