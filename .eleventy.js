85b76186c7778cb25c72d52d2046aafc99785927
Jan Mikeš <j.mikes@me.com>
2025-01-01 14:30:27 +0100

    Favicon + lightbox (gallery)

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({
-        "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js": "js/bootstrap.bundle.min.js",
-        "src/images": "images"
+        "src/favicon": "/",
+        "src/images": "images/",
+        "node_modules/bootstrap/dist/js": "js/",
+        "node_modules/glightbox/dist": "glightbox/"
     });
 
     return {
