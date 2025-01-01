327a961deac5fd0b65e2258a592033f32200e5b0
Jan Mikeš <j.mikes@me.com>
2025-01-01 12:56:00 +0100

    Sections + basic styling

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,7 @@
-module.exports = function(eleventyConfig) {
+export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({
-        "src/styles": "styles",
-        "node_modules/bootstrap/dist/css/bootstrap.min.css": "styles/bootstrap.css",
-        "node_modules/@webpixels/css/dist/index.css": "styles/webpixels.css",
-        "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js": "js/bootstrap.bundle.min.js"
+        "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js": "js/bootstrap.bundle.min.js",
+        "src/images": "images"
     });
 
     return {
